16be0946f65190bb4a8b2cc1a39a5dbcec90d543
Fernando <fernando.luiz92@hotmail.com>
2022-02-22 20:25:01 -0300

    Projeto inicial

---
+++ src/com/bancodio/entities/ContaComun.java
@@ -0,0 +1,21 @@
+package com.bancodio.entities;
+
+import java.util.Date;
+
+public abstract class ContaComun {
+	
+	int nrConta;
+	Date dtAbertura;
+	Date dtEncerramento;
+	Double saldo;
+	
+	public void sacar() {
+		
+	}
+	public void depositar() {
+		
+	}
+	public void transferir() {
+		
+	}
+}
---
+++ src/com/bancodio/entities/ContaCorrente.java
@@ -0,0 +1,12 @@
+package com.bancodio.entities;
+
+public class ContaCorrente extends ContaComun {
+	
+	@Override
+	public void depositar() {
+		// TODO Auto-generated method stub
+		super.depositar();
+	}
+	
+
+}
