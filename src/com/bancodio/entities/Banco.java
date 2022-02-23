aa43067857935b243dae1638c4d4d6b9491a2bec
Fernando <fernando.luiz92@hotmail.com>
2022-02-22 22:04:46 -0300

    Encapsulamento

---
+++ src/com/bancodio/entities/Banco.java
@@ -0,0 +1,13 @@
+package com.bancodio.entities;
+
+public class Banco {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ src/com/bancodio/entities/Cliente.java
@@ -0,0 +1,15 @@
+package com.bancodio.entities;
+
+public class Cliente {
+	
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
